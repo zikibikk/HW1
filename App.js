aa3706c38b71ec9685cdb71931736c6fe7e2ff38
Alina <zikibikk@gmail.com>
2023-10-03 16:30:23 +0300

    hw2

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredLogin, setEnteredLogin] = useState('');
+  const [enteredPasswd, setEnteredPasswd] = useState('');
+  const [hasError, setError] = useState(false);
+  const [showForm, setShowForm] = useState(true);
+
+  const login = 'admin';
+  const passwd = 'admin';
+
+  const logIn = () => {
+    if (enteredLogin===login && enteredPasswd == passwd) {
+      setShowForm(false);
+      setError(false);
+    }
+    else { setError(true); }
+  }
+
+  const logOut = () => {
+    setShowForm(true);
+    setError(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      alignContent: 'center',
+      justifyContent: 'center',
+      padding: 20,
+     }}>
+      { showForm ? (
+        <>
+        <Text style={{color: 'red' }}>
+          { hasError ? 'Неверный логин или пароль' : ''}
+        </Text> 
+        <TextInput 
+        style={{ padding: 15, backgroundColor: '#f5f5f5' }}
+        placeholder='Логин:'
+        onChangeText={a => setEnteredLogin(a)}/>
+        <TextInput 
+        style={{ padding: 15, backgroundColor: '#f5f5f5' }}
+        placeholder='Пароль:'
+        onChangeText={a => setEnteredPasswd(a)}/>
+        <Button
+        onPress={logIn}
+        title='Войти'
+      />
+        </>
+      )
+      : (
+        <>
+        <Text style={{color: 'green' }}>
+        Вы успешно вошли, {enteredLogin}
+        </Text>
+        <Button
+        onPress={logOut}
+        title='Выйти'
+      />
+      </>
+      )
+    }
     </View>
   );
 }
